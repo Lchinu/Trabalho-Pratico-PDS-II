dd44883ca496a9b80cf616f49946fa4d3970aad6
Leonardo Oliveira <94313134+Lchinu@users.noreply.github.com>
2022-11-22 14:47:00 -0300

    feat: ajustes formatação main

--- program/main.cpp
+++ program/main.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 
 #include "Conta.hpp"
 #include "Titular.hpp"
@@ -9,41 +9,23 @@
 
 using namespace std;
 
-void ExibeSaldo(Conta &conta){ //Estou recebendo uma referência constante pois ,o método olharSaldo é um método que não modifica nada na classe , ou seja , ele também é const . 
-    cout<<"O saldo da conta é : "<<conta.olharSaldo()<<endl;
+void ExibeSaldo(Conta &conta)
+{ // Estou recebendo uma referência constante pois ,o método olharSaldo é um método que não modifica nada na classe , ou seja , ele também é const .
+    cout << "O saldo da conta é : " << conta.olharSaldo() << endl;
 }
 
-
-int main(){
+int main()
+{
 
     Titular titular(Cpf("123.456.789-10"), "Douglas Lima");
-    
+
     Conta umaConta("123456", titular);
     umaConta.depositar(1000);
     umaConta.sacar(499);
 
     ExibeSaldo(umaConta);
 
-    Funcionario funcionario(Cpf("123.456.789-11"),"Riquelme Batista",1000);
-
-  
-    
-    
-
-
-   
-   
-   
-
-
-
-
-
-
-
-
-
-
+    Funcionario funcionario(Cpf("123.456.789-11"), "Riquelme Batista", 1000);
 
     return 0;
 }
