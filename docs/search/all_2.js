09d7f14b90298e055a66d6a074b702d9e5d49edd
Leonardo Oliveira <94313134+Lchinu@users.noreply.github.com>
2022-11-22 14:50:38 -0300

    docs: adicionado documentação doxygen

---
+++ docs/search/all_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['funcionario_0',['Funcionario',['../class_funcionario.html',1,'Funcionario'],['../class_funcionario.html#a806a76044140d8b9c599f106cb308d48',1,'Funcionario::Funcionario()']]]
+];
---
+++ docs/search/functions_8.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['validacaodecpf_0',['validacaoDeCpf',['../class_cpf.html#a4b3e230d3254faec62859ca1adf5d2a0',1,'Cpf']]]
+];
