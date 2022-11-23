3b8bdbdcd1c1c2b49ec6764cdd2ddc1fdbf5ccf7
Lucas Fernando <94313134+Lchinu@users.noreply.github.com>
2022-11-23 00:40:14 -0300

    Update Conta.hpp

--- include/Conta.hpp
+++ include/Conta.hpp
@@ -3,21 +3,24 @@
 #pragma once
 #include <string>
 #include "Titular.hpp"
-
-class Conta
-{
-public:
-    static int numeroDeContas;
-
-private:
-    std::string numeroConta;
-    Titular titular;
-    float saldo;
-
-public:
-    Conta(std::string numero, Titular titular);
-    void sacar(float valorASacar);
-    void depositar(float valorADepositar);
-    float olharSaldo() const; // Esse método não modifica nada , por isso usei o const
-    std::string olharNumeroConta();
+lass Conta{
+    protected:
+        Titular titular;
+        float saldo;
+        virtual float taxaSaque(){
+        	return 0.05;
+		}
+    public:
+        Conta(Titular t) : titular(t){saldo = 0;}
+        virtual void realizaSaque(float valorSaque);
+        void realizaDeposito(float valorDeposito);
+        float get_saldo(){return saldo;}
+        void imprimeSaldo(){
+        	cout << "Saldo de " << titular.get_nome() << ": " << saldo << " reais." << endl;
+		}
+        void transfere(string userDestino, float valor, map<string,Conta*>contas);
+        ~Conta(){cout << "Conta encerrada." << endl;}
+        Titular* get_titular(){
+            return &titular;
+        }
 };
