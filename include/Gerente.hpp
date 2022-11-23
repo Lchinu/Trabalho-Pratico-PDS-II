557e22dd422cccfd0aa8d89c09b6572f87553666
Lucas Fernando <94313134+Lchinu@users.noreply.github.com>
2022-11-23 00:37:51 -0300

    Update Gerente.hpp

--- include/Gerente.hpp
+++ include/Gerente.hpp
@@ -1,1 +1,11 @@
+#pragma once
+#include <string>
+#include "Funcionario.hpp"
 
+class Gerente : public Funcionario{
+    public:
+        Gerente(string n, int i, string c, float s) : Funcionario(n,i,c,s){}
+        float bonificacao() override{
+            return get_salario()*0.5;
+        }
+};
