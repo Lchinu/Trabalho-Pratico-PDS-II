96599d446e5c5ceb20d9f92cc1cab1ebc52aee9e
Lucas Fernando <94313134+Lchinu@users.noreply.github.com>
2022-11-23 00:29:54 -0300

    Update Funcionario.hpp

--- include/Funcionario.hpp
+++ include/Funcionario.hpp
@@ -9,7 +9,14 @@ class Funcionario : public Pessoa
 private:
 	float salario;
 
-public:
-	Funcionario(Cpf cpf, std::string nome, float salario);
-	std::string recuperaNome();
+ public:
+        Funcionario(string n, int i, string c, float s) : Pessoa(n,i,c),
+        salario(s)
+        {cout << "Funcionario criado com sucesso." << endl;}
+        float get_salario(){
+            return salario;
+        }
+        virtual float bonificacao(){
+            return 0;
+        }
 };
