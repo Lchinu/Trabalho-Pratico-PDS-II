1eea367ecf565246f3e6af672ce9cd7e7448c36f
Lucas Fernando <94313134+Lchinu@users.noreply.github.com>
2022-11-23 00:34:31 -0300

    Update Caixa.hpp

--- include/Caixa.hpp
+++ include/Caixa.hpp
@@ -1,1 +1,11 @@
+#pragma once
+#include <string>
+#include "Funcionario.hpp"
 
+class Caixa : public Funcionario{
+    public:
+        Caixa(string n, int i, string c, float s) : Funcionario(n,i,c,s){}
+        float bonificacao() override{
+            return get_salario()*0.1;
+        }
+};
