62ba3586e79209eb573881efa8dd6a6d93b9d77c
Leonardo Oliveira <94313134+Lchinu@users.noreply.github.com>
2022-11-24 21:08:35 -0300

    feat: adicionado try catch

--- program/main.cpp
+++ program/main.cpp
@@ -10,29 +10,53 @@
 
 using namespace std;
 
-int main(){
-    Banco banco;
-	
-	Titular t1("Matheus", 23, "13535745632","mathewshq", "2607");
-	ContaUniversitaria c1(t1);
-    banco.adicionaConta(c1);
-    
-	Titular t2("Paulo", 22, "12782581678", "pauloh", "8600");
-	ContaPoupanca p1(t2);
-    banco.adicionaConta(p1);
+int main()
+{
 
-    Conta* contaLogada = banco.login();
+    try
+    {
+        Banco banco;
 
-    int operacao = 0;
-    do{
-    operacao = banco.selecionaOperacao();
+        Titular t1("Matheus", 23, "13535745632", "mathewshq", "2607");
+        ContaUniversitaria c1(t1);
+        banco.adicionaConta(c1);
 
-    banco.realizaOperacao(operacao, contaLogada);
+        Titular t2("Paulo", 22, "12782581678", "pauloh", "8600");
+        ContaPoupanca p1(t2);
+        banco.adicionaConta(p1);
 
-    if (operacao == 5){
-        system("cls");
-        contaLogada = banco.login();
+        Conta *contaLogada = banco.login();
+
+        int operacao = 0;
+
+        do
+        {
+            try
+            {
+                operacao = banco.selecionaOperacao();
+
+                banco.realizaOperacao(operacao, contaLogada);
+
+                if (operacao == 5)
+                {
+                    system("cls");
+                    contaLogada = banco.login();
+                }
+
+            }
+            catch (exception &e)
+            {
+                cout << e.what() << endl;
+                cout << "Aperte qualquer tecla para continuar." << endl;
+                cin.get();
+            }
+
+        } while (operacao != 6);
     }
-    
-    }while(operacao != 6);
+    catch (exception &e)
+    {
+        cout << e.what() << endl;
+        cout << "Aperte qualquer tecla para sair." << endl;
+        cin.get();
+    }
 }
