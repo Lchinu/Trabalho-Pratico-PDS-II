d364375f88a0196b7b011b942566fa3ee14b268f
Lucas Fernando <94313134+Lchinu@users.noreply.github.com>
2022-11-29 00:19:19 -0300

    Update main.cpp

--- program/main.cpp
+++ program/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 #include "banco.hpp"
 #include "conta.hpp"
 #include "contaPoupanca.hpp"
@@ -7,56 +6,38 @@
 #include "pessoa.hpp"
 #include "titular.hpp"
 #include "contaUniversitaria.hpp"
+#include "transacao.hpp"
+
 
 using namespace std;
 
-int main()
-{
+int main(){
+    Banco banco;
+	
+	Titular t1("Matheus", 23, "13535745632","mathewshq", "2607");
+	ContaUniversitaria c1(t1);
+    banco.adicionaConta(c1);
+    
+	Titular t2("Paulo", 22, "12782581678", "pauloh", "8600");
+	ContaPoupanca p1(t2);
+    banco.adicionaConta(p1);
 
-    try
-    {
-        Banco banco;
+   
+    Conta* contaLogada = banco.login();
 
-        Titular t1("Matheus", 23, "13535745632", "mathewshq", "2607");
-        ContaUniversitaria c1(t1);
-        banco.adicionaConta(c1);
+    int operacao = 0;
+    while(1){
+    operacao = banco.selecionaOperacao();
 
-        Titular t2("Paulo", 22, "12782581678", "pauloh", "8600");
-        ContaPoupanca p1(t2);
-        banco.adicionaConta(p1);
+    banco.realizaOperacao(operacao, contaLogada);
 
-        Conta *contaLogada = banco.login();
-
-        int operacao = 0;
-
-        do
-        {
-            try
-            {
-                operacao = banco.selecionaOperacao();
-
-                banco.realizaOperacao(operacao, contaLogada);
-
-                if (operacao == 5)
-                {
-                    system("cls");
-                    contaLogada = banco.login();
-                }
-
-            }
-            catch (exception &e)
-            {
-                cout << e.what() << endl;
-                cout << "Aperte qualquer tecla para continuar." << endl;
-                cin.get();
-            }
-
-        } while (operacao != 6);
+    if (operacao == 5){
+        system("cls");
+        contaLogada = banco.login();
+    }else if(operacao == 6 || operacao == 0){
+        break;
+        }   
     }
-    catch (exception &e)
-    {
-        cout << e.what() << endl;
-        cout << "Aperte qualquer tecla para sair." << endl;
-        cin.get();
-    }
+
+return 0;
 }
