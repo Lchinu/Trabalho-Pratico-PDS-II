67d5f04356839feaa49b81c69b01d5b1202146ba
Lucas Fernando <94313134+Lchinu@users.noreply.github.com>
2022-11-29 00:19:56 -0300

    Add files via upload

---
+++ include/banco.hpp
@@ -0,0 +1,107 @@
+/**
+ * @file banco.hpp
+ * @author Lucas Fernando (lchinuzinhoplay@gmail.com)
+ * @brief Classe principal do projeto caixa eletrônico.
+ * @version 0.1
+ * @date 2022-11-25
+ * @details A classe banco é responsãvel por organizar as operações que 
+ * constituem um caixa eletrônico como um todo e por checar se as contas 
+ * realmente existem e cria propriamente o caixa, é a classe principal do projeto. 
+ * 
+ * 
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef __BANCO_HPP__
+#define __BANCO_HPP__
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "conta.hpp"
+#include <map>
+#include <transacao.hpp>
+
+/**
+ * @brief Classe que cria contas e organiza opções escolhidas.
+ * @details Implementa ao sistema bancário as contas criadas pelos usuários,
+ * além de incrementar as funcionalidades e dar a opção de escolha de uma delas.
+ */
+class Banco
+{
+public:
+  
+    /**
+     * @brief Vetor com os nomes de usuarios existentes, para conferencia no login
+     * 
+     */
+    std::vector<std::string> Users;   
+    /**
+     * @brief Busca uma conta a partir de um nome de usuario existente(ponteiro para conta)
+     * 
+     */
+    std::map<std::string, Conta *> Contas; 
+    /**
+     * @brief Criação do saldo do caixa eletrônico, feito pois os caixas não possuem dinheiro infinito.
+     * 
+     */
+    float saldo;
+    /**
+     * @brief Registra as transações feitas pelo usuário
+     * 
+     */
+std::vector<Transacao>transacoes;
+public:
+/**
+ * @brief Constrói o objeto Banco.
+ * @details Inicia a estrutura do código com o os usuários, tipos de conta 
+ * e saldo, assim como as operações disponíveis.
+ * 
+ */
+    Banco();
+    /**
+     * @brief Adiciona conta criada ao banco, por referência
+     * 
+     * @param conta Classe conta contendo: dados do usuário, saldo e tipo de conta.
+     */
+    void adicionaConta(Conta &conta); 
+    /**
+     * @brief Criação do sistema de login
+     * @details Sistema criado para que seja possível inicar e encerrar sessão em diferentes 
+     * tipos de contas.
+     * @return Conta*  retorna a conta que foi logada
+     */
+    Conta *login();
+    /**
+     * @brief O usuario logado escolhe a operacao que será realzada no caixa eletrônico
+     * 
+     * @return int 
+     */
+    int selecionaOperacao();  
+    /**
+     * @brief após selecionada a opção desejada, o caixa realiza a operação
+     * @details As operações são feitas com base na conta atual logada, diferenciando-se
+     * entre os dois tipos existentes, como saque.
+     * 
+     * @param opcao O usuário deve escolher qual opereação deseja realizar
+     * @param conta A operação é destinada a conta 
+     * @return * void 
+     */
+    void realizaOperacao(int opcao, Conta *conta);
+    /**
+     * @brief Saldo atual do banco, para não ter dinheiro infinito para saque.
+     * 
+     * @return float 
+     */
+    float get_saldo();
+    /**
+     * @brief Responsável por reunir todas as transações feitas.
+     * @details Essa função foi criada especificamente para reunir todas as transações
+     * em um único lugar e fornecer maior organização.
+     * 
+     */
+    void imprimeExtrato();
+};
+
+#endif
---
+++ include/titular.hpp
@@ -0,0 +1,69 @@
+/**
+ * @file titular.hpp
+ * @author Lucas Fernando (lchinuzinhoplay@gmail.com)
+ * @brief Classe que cria as contas titulares
+ * @version 0.1
+ * @date 2022-11-25
+ * @details Responsável por unir as informações dos usuários com o tipo de conta,
+ * além de pedir um nickname e uma senha para a conta.
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef __TITULAR_HPP__
+#define __TITULAR_HPP__
+
+#include <iostream>
+#include <string>
+#include "pessoa.hpp"
+/**
+ * @brief Classe responsável pela criação do login
+ * @details Como toda conta bancária possui um titular, com login e senha,
+ * essa classe é responsável pela criação destes atributos para cada Pessoa.
+ * 
+ */
+class Titular : public Pessoa
+{
+private:
+/**
+ * @brief Criação da senha e usuário dos titulares.
+ * 
+ */
+    std::string usuario, senha;
+
+public:
+/**
+ * @brief Construtor do objeto titular
+ * @details Constrói um usuário titular com os dados dos usuários e atribui 
+ * a eles um login e uma senha
+ * 
+ * @param n Nome do titular
+ * @param i Idade do titular
+ * @param c Cpf do titular
+ * @param u Usuário do titular
+ * @param s Senha do titular
+ */
+    Titular(std::string n, int i, std::string c, std::string u, std::string s);
+    /**
+     * @brief Construtor do objeto titular
+     * 
+     * @param n Nome do titular
+     * @param i Idade do titular
+     * @param c Cpf do titular
+     */
+    Titular(std::string n, int i, std::string c);
+    /**
+     * @brief Definido a senha do usuário
+     * 
+     * @return retorna a senha
+     */
+    std::string get_senha();
+    /**
+     * @brief Definido o usuário titular da conta.
+     * 
+     * @return std::string 
+     */
+    std::string get_usuario();
+};
+
+#endif
